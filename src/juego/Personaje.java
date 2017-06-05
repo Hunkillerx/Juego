2965d1ddf4d6654b2722fe7e2c9bd99826c7cc94
Josex <Josex@DESKTOP-D64FD4S>
2017-06-04 20:32:23 -0500

    Personaje

---
+++ src/juego/Personaje.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego;
+
+import java.util.List;
+
+/**
+ *
+ * @author Jose Diaz
+ */
+public class Personaje {
+    
+    private String nombre;
+    private int tipo;
+    private byte nivel;
+    private byte ataque;
+    private byte defensa;
+    private byte hp;
+    private int estado;
+    private int currentHp;
+    private Habilidad ataque1;
+    private Habilidad ataque2;
+    private Habilidad ataque3;
+    private List almacenHabilidades;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public byte getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(byte nivel) {
+        this.nivel = nivel;
+    }
+
+    public byte getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(byte ataque) {
+        this.ataque = ataque;
+    }
+
+    public byte getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(byte defensa) {
+        this.defensa = defensa;
+    }
+
+    public byte getHp() {
+        return hp;
+    }
+
+    public void setHp(byte hp) {
+        this.hp = hp;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public int getCurrentHp() {
+        return currentHp;
+    }
+
+    public void setCurrentHp(int currentHp) {
+        this.currentHp = currentHp;
+    }
+
+    public Habilidad getAtaque1() {
+        return ataque1;
+    }
+
+    public void setAtaque1(Habilidad ataque1) {
+        this.ataque1 = ataque1;
+    }
+
+    public Habilidad getAtaque2() {
+        return ataque2;
+    }
+
+    public void setAtaque2(Habilidad ataque2) {
+        this.ataque2 = ataque2;
+    }
+
+    public Habilidad getAtaque3() {
+        return ataque3;
+    }
+
+    public void setAtaque3(Habilidad ataque3) {
+        this.ataque3 = ataque3;
+    }
+
+    public List getAlmacenHabilidades() {
+        return almacenHabilidades;
+    }
+
+    public void setAlmacenHabilidades(List almacenHabilidades) {
+        this.almacenHabilidades = almacenHabilidades;
+    }
+    
+    
+}
