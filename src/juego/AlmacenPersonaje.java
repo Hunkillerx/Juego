6d2290d2ac495872bae1ad89a683cdd463917383
SALA309 <SALA309@EAN2523.universidad.ean>
2017-06-16 16:42:37 -0500

    Grafica batalla y ajustes de algunos metodos asdaasd

--- src/juego/AlmacenPersonaje.java
+++ src/juego/AlmacenPersonaje.java
@@ -29,7 +29,7 @@ public class AlmacenPersonaje {
     Habilidad ataque7 = new Habilidad(10, 2, 3, "Mordisco");
     Habilidad ataque8 = new Habilidad(20, 2, 2, "Congelacion");
     Habilidad ataque9 = new Habilidad(30, 1, 3, "Cuchillada");
-    Habilidad ataque10 = new Habilidad(17, 2, 3, "Zarpazo");
+    Habilidad ataque10 = new Habilidad(16, 2, 3, "Zarpazo");
 
     /**
      * Aqui estan almacenados los monstruos.
@@ -56,15 +56,9 @@ public class AlmacenPersonaje {
      * @param numero numero que se ingresa de forma aleatoria.
      */
     public void vincular(int numero) {
-        if (numero == 1) {
+        
             monstruo1 = almacenMonstruos[0];
-        } else if (numero == 2) {
-            monstruo1 = almacenMonstruos[1];
-        } else if (numero == 3) {
-            monstruo1 = almacenMonstruos[2];
-        } else if (numero == 4) {
-            monstruo1 = almacenMonstruos[3];
-        }
+        
     }
 
     private Personaje heroe = new Personaje("Kirito", 1, 5, 20, 25, 120, 0, 120, false, false, ataque1, ataque9, ataque6);
